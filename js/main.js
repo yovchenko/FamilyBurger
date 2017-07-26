9f3ce8969d21b4f3df63a5e41bb73bb730162ec8
vladimir <yovchenko.w@gmail.com>
2017-07-27 00:59:46 +0300

    delete function onresize

--- js/main.js
+++ js/main.js
@@ -1,19 +1,10 @@
 $( document ).ready(function() {
     var $carousel = $('.carousel');
     $carousel.carousel();
-         var $item = $('.carousel .item'); 
-var $wHeight = $(window).height();
-$item.eq(0).addClass('active');
-$item.height($wHeight); 
-$item.addClass('full-screen');
-$(window).on('resize', function (){
-  $wHeight = $(window).height();
-  $item.height($wHeight);
-});
-         $carousel.carousel({
+    $carousel.carousel({
          interval: 3500,
          pause: "false"
-          });
+    });
 
 var $itemNum;    
 $('.carousel-control-next').click(function(){
