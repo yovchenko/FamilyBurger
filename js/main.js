0082f5de18a05ff0b3bd9e7b11925ef5e755e01b
vladimir <yovchenko.w@gmail.com>
2017-07-26 22:28:20 +0300

    changed carousel width

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 $( document ).ready(function() {
+         $('.carousel').carousel();
+         var $item = $('.carousel .item'); 
+var $wHeight = $(window).height();
+$item.eq(0).addClass('active');
+$item.height($wHeight); 
+$item.addClass('full-screen');
+$(window).on('resize', function (){
+  $wHeight = $(window).height();
+  $item.height($wHeight);
+});
          $('.carousel').carousel({
-         interval: 3000
+         interval: 3500,
+         pause: "false"
           });
 });
+   
