6faaaf6321f54ff0fd7a30af76e8362f631deb73
vladimir <yovchenko.w@gmail.com>
2017-07-28 23:01:29 +0300

    added images preloader

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
 $( document ).ready(function() {
 function preloader() {
-	if (document.images) {
+	if (document.images && window.matchMedia('(min-width: 769px)').matches) {
 		var img1 = new Image();
 		var img2 = new Image();
         var img3 = new Image();
-        var img4 = new Image();
-		var img5 = new Image();
-		var img6 = new Image();
-
 		img1.src = "http://yovchenko.github.io/familyBurger/images/slide-1-lg.jpg";
 		img2.src = "http://yovchenko.github.io/familyBurger/images/slide-2-lg.jpg";
         img3.src = "http://yovchenko.github.io/familyBurger/images/slide-3-lg.jpg";
-        img4.src = "http://yovchenko.github.io/familyBurger/images/slide-1-md.jpg";
+    }
+    
+    else if (document.images && window.matchMedia('(max-width: 768px)').matches) {
+        var img4 = new Image();
+		var img5 = new Image();
+        var img6 = new Image();
+		img4.src = "http://yovchenko.github.io/familyBurger/images/slide-1-md.jpg";
 		img5.src = "http://yovchenko.github.io/familyBurger/images/slide-2-md.jpg";
         img6.src = "http://yovchenko.github.io/familyBurger/images/slide-3-md.jpg";
-	}
+    }
 }
 function addLoadEvent(func) {
 	var oldonload = window.onload;
