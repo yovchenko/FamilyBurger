2bda283896d02c5a1e46f3d9317ecd60822b45e8
vladimir <yovchenko.w@gmail.com>
2017-07-28 21:15:51 +0300

    deleted preload

--- js/main.js
+++ js/main.js
@@ -1,20 +1,4 @@
 $( document ).ready(function() {
-var images = new Array()
-function preload() {
-    for (i = 0; i < preload.arguments.length; i++) {
-        images[i] = new Image()
-        images[i].src = preload.arguments[i]
-    }
-}
-preload(
-    "http://yovchenko.github.io/familyBurger/images/slide-1-lg.jpg",
-    "http://yovchenko.github.io/familyBurger/images/slide-2-lg.jpg",
-    "http://yovchenko.github.io/familyBurger/images/slide-3-lg.jpg",
-    "http://yovchenko.github.io/familyBurger/images/slide-1-md.jpg",
-    "http://yovchenko.github.io/familyBurger/images/slide-2-md.jpg",
-    "http://yovchenko.github.io/familyBurger/images/slide-3-md.jpg"
-)
-
 var $carousel = $('.carousel');
     $carousel.carousel();
     $carousel.carousel({
