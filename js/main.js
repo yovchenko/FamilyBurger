9c290db083ae4b1f6a07f1759935ec4f8ea81fcb
vladimir <yovchenko.w@gmail.com>
2017-07-28 22:41:18 +0300

    test

--- js/main.js
+++ js/main.js
@@ -1,4 +1,35 @@
 $( document ).ready(function() {
+function preloader() {
+	if (document.images) {
+		var img1 = new Image();
+		var img2 = new Image();
+        var img3 = new Image();
+        var img4 = new Image();
+		var img5 = new Image();
+		var img6 = new Image();
+
+		img1.src = "http://yovchenko.github.io/familyBurger/images/slide-1-lg.jpg";
+		img2.src = "http://yovchenko.github.io/familyBurger/images/slide-2-lg.jpg";
+        img3.src = "http://yovchenko.github.io/familyBurger/images/slide-3-lg.jpg";
+        img4.src = "http://yovchenko.github.io/familyBurger/images/slide-1-md.jpg";
+		img5.src = "http://yovchenko.github.io/familyBurger/images/slide-2-md.jpg";
+        img6.src = "http://yovchenko.github.io/familyBurger/images/slide-3-md.jpg";
+	}
+}
+function addLoadEvent(func) {
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function') {
+		window.onload = func;
+	} else {
+		window.onload = function() {
+			if (oldonload) {
+				oldonload();
+			}
+			func();
+		}
+	}
+}
+addLoadEvent(preloader);
 var $carousel = $('.carousel');
     $carousel.carousel();
     $carousel.carousel({
