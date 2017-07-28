887ddc15527406260d927bad1026b31381c2b212
vladimir <yovchenko.w@gmail.com>
2017-07-28 19:46:08 +0300

    changed css preload

--- js/main.js
+++ js/main.js
@@ -1,29 +1,5 @@
 $( document ).ready(function() {
-    function preloader() {
-	if (document.getElementById) {
-		document.getElementById("preload-01").style.background = "url('../images/slide-1-lg.jpg') no-repeat -9999px -9999px";
-		document.getElementById("preload-02").style.background = "url('../images/slide-2-lg.jpg') no-repeat -9999px -9999px";
-        document.getElementById("preload-03").style.background = "url('../images/slide-3-lg.jpg') no-repeat -9999px -9999px";
-        document.getElementById("preload-04").style.background = "url('../images/slide-1-md.jpg') no-repeat -9999px -9999px";
-		document.getElementById("preload-05").style.background = "url('../images/slide-2-md.jpg') no-repeat -9999px -9999px";
-		document.getElementById("preload-06").style.background = "url('../images/slide-3-md.jpg') no-repeat -9999px -9999px";
-	}
-}
-function addLoadEvent(func) {
-	var oldonload = window.onload;
-	if (typeof window.onload != 'function') {
-		window.onload = func;
-	} else {
-		window.onload = function() {
-			if (oldonload) {
-				oldonload();
-			}
-			func();
-		}
-	}
-}
-addLoadEvent(preloader)
-    var $carousel = $('.carousel');
+var $carousel = $('.carousel');
     $carousel.carousel();
     $carousel.carousel({
          interval: 3500,
