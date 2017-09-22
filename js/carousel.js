5c2215e69dcddb030209db9ddcec4faff0d48d37
vladimir <yovchenko.w@gmail.com>
2017-09-22 18:28:15 +0300

    changed carousel function

--- js/carousel.js
+++ js/carousel.js
@@ -1,11 +1,5 @@
  $(document).ready(function() {
     let $carousel = $('.carousel');
-    $carousel.carousel();
-    $carousel.carousel({
-        interval: 3500,
-        pause: "false",
-        wrap: true
-    });
     $carousel.swiperight(function () {
         $(this).carousel('prev');
     });
