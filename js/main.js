498a218ba58bcc02e5fc7969d8e74da266537682
yovchenko <yovchenko.w@gmail.com>
2018-02-09 22:34:00 +0200

    test

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ var scrollHeight = $(document).height(),
   header = document.getElementsByClassName('navbar')[0],
   body = document.getElementsByClassName('body')[0];
 
+  document.addEventListener('touchmove', function (e) {
+    if (scrollPosition > $(document).height()) {
+    e.preventDefault();
+    }
+  }, false);
+
 if ($(window).height() === scrollPosition) {
   header.classList.remove('scrolled-nav');
 } else if (scrollPosition >= $(document).height()) {
@@ -33,15 +39,9 @@ $(window).scroll(function () {
   }
   if (scrollPosition >= $(document).height()) {
     body.classList.add('bottom');
-    document.addEventListener('touchmove', function (e) {
-      e.preventDefault();
-    }, false);
   }
   if ($(this).height() === scrollPosition) {
     header.classList.remove('scrolled-nav');
-    document.addEventListener('touchmove', function (e) {
-      e.preventDefault();
-    }, false);
   }
 });
 
